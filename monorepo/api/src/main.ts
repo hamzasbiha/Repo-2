47c8f97ac0b933a5538c79bdf9ee396d1163d248
ha12mza <121032618+hamzasbiha@users.noreply.github.com>
2023-11-26 15:36:46 +0100

    commented session

--- monorepo/api/src/main.ts
+++ monorepo/api/src/main.ts
@@ -1,23 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
-import * as session from 'express-session';
 import * as passport from 'passport';
-import { ConfigService } from '@nestjs/config';
+// import * as express from 'express';
+// import * as session from 'express-session';
+// import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService);
-  app.use(
-    session({
-      secret: configService.get('SEASSIONSECRET'),
-      saveUninitialized: false,
-      resave: false,
-      cookie: {
-        maxAge: 60000,
-      },
-    }),
-  );
+  // const configService = app.get(ConfigService);
+  // app.use(
+  //   session({
+  //     secret: configService.get('SEASSIONSECRET'),
+  //     saveUninitialized: false,
+  //     resave: false,
+  //     cookie: {
+  //       maxAge: 60000,
+  //     },
+  //   }),
+  // );
   app.use(passport.initialize());
   app.enableCors();
   app.useGlobalPipes(
